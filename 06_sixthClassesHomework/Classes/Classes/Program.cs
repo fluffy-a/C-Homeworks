8eb92c96d86a3cf9c3dbf350df8d8309808a4343
fluffy-a <atwefashionit@gmail.com>
2020-04-01 02:38:14 +0200

    strings and methods

---
+++ 06_sixthClassesHomework/Classes/Classes/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace Classes
+{
+    //public class Dog
+
+
+    //            Task 7
+    //Create a class Dog
+    //Add properties: Name, race, color
+    //The dog needs to have:
+    //Eat method that returns message: The dog is now eating
+    //Play method returning a message : The dog is now playing
+    //ChaseTail method that returns a message: Dog is now chasing its tail.
+    //The user needs to create the dog object by inputs and then given an option to choose one of the actions mentioned above.
+
+
+
+    //{
+
+
+        //// Properties 
+        //public string Name;
+        //public string Race;
+        //public string Color;
+
+        ////Method
+        //public void Eat()
+        //{
+        //    Console.WriteLine("The dog is now eating");
+
+        //}
+        //public void Play()
+        //{
+        //    Console.WriteLine("The dog is now playing");
+        //}
+
+        //public void ChaseTail()
+        //{
+        //    Console.WriteLine("Dog is now chasing its tail.");
+        //}
+
+
+
+
+        //    Create a class Human
+        //Add properties: FirstName, LastName, Age
+        //Create a method called GetPersonStats that returns the full name of the human as well as their age
+        //Create an object human by asking the user to fill the required information
+        //Call the GetPersonStats method and print the result in the console after the object is created
+
+            public class Human
+        {
+
+
+            
+            //Properties
+            public string FirstName;
+            public string LastName;
+            //private string Age;
+
+
+
+            //Method
+            public void GetPersonStats()
+            {
+                Console.WriteLine($" Hi, I am {FirstName} {LastName} , nice to meet u");
+                //HowOld(DateTime.Today);
+            }
+            //private void HowOld(DateTime today)
+            //{
+            //    if (today.Month < DateOfBirth.Month)
+            //        Age = today.Year - DateOfBirth.Year - 1;
+            //}
+
+
+        }
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //Dog dog = new Dog();
+
+                //Console.WriteLine("Enter name for a dog");
+                //dog.Name = Console.ReadLine();
+
+                //Console.WriteLine("Enter race for a dog");
+                //dog.Race = Console.ReadLine();
+
+
+                //Console.WriteLine("Enter color for a dog");
+                //dog.Color = Console.ReadLine();
+
+                //while (true)
+                //{
+                //    Console.WriteLine("Does dog is eating? y/n");
+                //    string answer = Console.ReadLine();
+                //    if (answer == "y")
+                //    {
+                //        dog.Eat();
+                //        break;
+                //    }
+
+                //    Console.WriteLine("Does dog is playing now? y/n ");
+
+                //    string answer1 = Console.ReadLine();
+                //    if (answer1 == "y")
+                //    {
+                //        dog.Play();
+                //        break;
+                //    }
+
+                //    Console.WriteLine("Does dog chasing his tail? y/n");
+                //    string answer3 = Console.ReadLine();
+                //    if (answer3 == "y")
+                //    {
+                //        dog.ChaseTail();
+                //        break;
+                //    }
+                //}
+
+
+                //Create an object human by asking the user to fill the required information
+                //class
+                Human human = new Human();
+
+                Console.WriteLine("Tell me your name");
+                human.FirstName = Console.ReadLine();
+
+                Console.WriteLine("Tell me your last name");
+                human.LastName = Console.ReadLine();
+
+                //Console.WriteLine("Tell me your age");
+                //human.Age = Console.ReadLine(); private
+
+
+                //Call the GetPersonStats method and print the result in the console after the object is created
+
+                human.GetPersonStats();
+
+
+
+
+                
+            //}
+        }
+    }
+    
+}
---
+++ 06_sixthClassesHomework/Strings/Homework/Homework/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            //* Print individual characters of the string in reverse order. *Example: we love c# *Expected Output : #c evol ew
+            //string lettersWords = "We,love,C#";
+
+            //string wordLetters = "We love C#";
+            //string reverse = "";
+
+            //for (int i = wordLetters.Length-1; i>=0 ; i--)
+            //{
+            //    reverse = reverse + wordLetters[i];
+            //}
+            //Console.WriteLine(reverse);
+
+
+            //Task 2
+            //* Count the total number of words in a string entered by the user. * 
+
+            //Console.WriteLine("Input a quote that u like it");
+            //string InputFromUser = Console.ReadLine();
+
+            //string[] words = InputFromUser.Trim().Split(' ');
+
+            //Console.WriteLine($"The number of words in the quote is  {words.Length}");
+
+
+            //Task 3
+            //Find maximum occurring character in a string* 
+            //Example: "We want this situation with covid-19 to ends!" * 
+            //Expected Output: The highest frequency of character 't' appears number of times : 6
+
+
+            string letterAboutSituation = "We want this situation with covid-19 to ends!";
+            char[] sentence = letterAboutSituation.ToCharArray();
+            char sign = ' ';
+            int count = 0;
+            int j;
+            int finalcounter = 0;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                count = 1;
+                for (j = i + 1; j < sentence.Length; j++)
+                {
+                    if (sentence[i] == sentence[j] && sentence[i] != ' ')
+                    {
+                        count++;
+                        if (count >= finalcounter)
+                        {
+                            finalcounter = count;
+                            sign = sentence[j];
+                        }
+                    }
+
+
+                }
+            }
+
+            Console.WriteLine(sentence);
+            Console.WriteLine($"The highest frequency of {sign} appears number of times :  {finalcounter}");
+
+
+
+            //Task 4    
+            //"The whole group of G1 loves C#, we are sure that with their deducation and passion they will be successfull programers soon!"
+            //* Print the whole text after "," in the console. *Bonus - Try to do that without counting the characters till "," by yourself :)
+
+            string group = "The whole group of G1 loves C#, we are sure that with their deducation and passion they will be successfull programers soon!";
+
+            var chars = new[] {','};
+
+            string[] afterChar = group.Split(chars);
+
+
+            Console.WriteLine(afterChar[1]); // index 0 is before (,), index 1 is after (,)
+
+
+
+            //Task 5
+            //* Display the Day properties(year, month, day, hour, minute, second) from the today's date. *Expected Output : *year = 2020
+            //* month = 3
+            //* day = 26
+            //* hour = 23
+            //* minute = 12
+            //* second = 22
+
+            var today = DateTime.Now;
+            Console.WriteLine(today);
+
+
+            int day = today.Day;
+            int month = today.Month;
+            int year = today.Year;
+            int hour = today.Hour;
+            int minute = today.Minute;
+            int second = today.Second;
+
+            Console.WriteLine($"{day}Day {month}Month {year}Year ,{hour}:{minute}:{second}sec");
+
+
+            //Task 6
+            // *Find the leap years between 2008 and 2020. * Expected output: *2008 is a leap year. 
+            //* 2012 is a leap year. * 2016 is a leap year. * 2020 is a leap year.
+            //* Hint - make some researches of DateTime methods, you will find interesting ones that might help you;)
+
+
+
+            int year1;
+
+            for (year1 = 2008; year1 <= 2020; year1++)
+            {
+                if (DateTime.IsLeapYear(year1))
+                {
+                    Console.WriteLine($"{year1} is leap year!");
+                }
+            }
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
