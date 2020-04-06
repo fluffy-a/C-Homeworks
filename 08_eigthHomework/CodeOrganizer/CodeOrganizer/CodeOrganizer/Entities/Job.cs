4e144dc52b851fac03007fdcde839b7b1110d80a
fluffy-a <atwefashionit@gmail.com>
2020-04-06 02:20:24 +0200

    /

---
+++ 08_eigthHomework/CodeOrganizer/CodeOrganizer/CodeOrganizer/Entities/Job.cs
@@ -0,0 +1,16 @@
+﻿
+using CodeOrganizer.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeOrginizer.Entities
+{
+    public class Job
+    {
+        public string CompanyName { get; set; }
+        public Address Address { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
---
+++ 08_eigthHomework/CodeOrganizer/CodeOrganizer/CodeOrganizer/Program.cs
@@ -0,0 +1,58 @@
+﻿using CodeOrganizer.Enum;
+using CodeOrginizer.Entities;
+using PersonServices;
+using System;
+
+
+
+
+namespace CodeOrginizer
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person john = new Person("John", 22);
+            john.Address = new Address();
+            john.Address.Name = "Wall street";
+            john.Address.Number = 24;
+            john.SSN = SSNGenerate.GenerateSSN();
+            john.PrintPerson();
+
+            Console.WriteLine("------------------------------------");
+
+            Address bobsAdress = new Address()
+            {
+                Name = "Next To Wallstreet",
+                Number = 1000
+            };
+            Address bobWorkingAdress = new Address()
+            {
+                Name = "Two streets after Wallstreet",
+                Number = 101
+            };
+            Person bob = new Person()
+            {
+                Name = "Bob",
+                Age = 55,
+                Address = bobsAdress,
+                SSN = SSNGenerate.GenerateSSN(),
+                Job = new Job()
+                {
+                    Address = bobWorkingAdress,
+                    CompanyName = "G1",
+                    Role = Role.Designer
+                }
+            };
+
+            bob.DevelopersSeacret();
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
