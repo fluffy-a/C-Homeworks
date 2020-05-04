c19dff9c1cefd54a6131c6178e92192d2f948788
fluffy-a <atwefashionit@gmail.com>
2020-05-04 19:50:34 +0200

    Calculator

---
+++ 10_TenHomework/11. Exercise for Calculator Max and Avg/Calculator/Calculator/Helpers/HelperMethod.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Calculator.Helpers
+{
+    internal static class HelperMethods
+    {
+        internal static decimal CalculateAverage(List<int> numbers)
+        {
+            int sum = 0;
+            numbers.ForEach(number => sum += number);
+            return Convert.ToDecimal(sum) / Convert.ToDecimal(numbers.Count);
+        }
+
+        internal static int FindMaxNumber(List<int> numbers)
+        {
+            int max = 0;
+            foreach (var number in numbers)
+            {
+                if (number > max)
+                    max = number;
+            }
+            return max;
+        }
+
+        internal static int FindMinNumber(List<int> numbers)
+        {
+            int min = numbers[0];
+            foreach (var number in numbers)
+            {
+                if (number<min)
+                
+                 min = number;
+                
+            }
+
+            return min;
+
+        }
+
+        internal static int Sum(List<int> numbers)
+        {
+            int sum = 0;
+            foreach (var number in numbers)
+            {
+                sum +=number;
+            }
+            return sum;
+        }
+
+        
+
+        
+    }
+}
---
+++ 10_TenHomework/11. Exercise for Calculator Max and Avg/Calculator/Calculator/Models/Calc.cs
@@ -0,0 +1,32 @@
+﻿using Calculator.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Calculator.Models
+{
+    public static class Calc
+    {
+        public static decimal Avg(List<int> numbers)
+        {
+            return HelperMethods.CalculateAverage(numbers);
+        }
+        public static int Max(List<int> numbers)
+        {
+            return HelperMethods.FindMaxNumber(numbers);
+        }
+
+        public static int Min(List<int> numbers)
+        {
+            return HelperMethods.FindMinNumber(numbers);
+        }
+
+        //Square Function x2
+        public static int Sum(List<int> numbers)
+        {
+            return HelperMethods.Sum(numbers);
+        }
+
+    }
+}
